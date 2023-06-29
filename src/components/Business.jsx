544922e15cae8ef8920289ba79015199217eb003
light-roast <echeverri121@gmail.com>
2023-06-29 11:04:23 -0500

    Import Business comp inside App comp - Working fine

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -26,15 +26,3 @@ export default function Business(props) {
         </>
     )
 }
-
-const hardCoreBusiness = {
-    imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-};
