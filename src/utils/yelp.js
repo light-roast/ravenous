92205277a66f5c0e1946c32a01f73127e0d07f54
Daniel Echeverri <echeverri121@gmail.com>
2023-07-27 17:55:57 -0500

    Siplifiy fetch function so that cñorQluza stands

--- src/utils/yelp.js
+++ src/utils/yelp.js
@@ -1,5 +1,7 @@
 async function yelp(searchTerm, location, sortOption, api_key){
     const key = `Bearer ${api_key}`;
+    const urlWithSort = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${location}&term=${searchTerm}&sort_by=${sortOption}`;
+    const url = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${location}&term=${searchTerm}`;
     const options = {
         method: 'GET',
         headers: {
@@ -9,11 +11,14 @@ async function yelp(searchTerm, location, sortOption, api_key){
       };
     try {
         if (sortOption === 'no') {
-            const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${location}&term=${searchTerm}`, options);
-            if (response.ok) {
-                const jsonResponse = await response.json();
-                if (jsonResponse.businesses) {
-                    return jsonResponse.businesses.map(business => ({
+            const response = await fetch(url, options);
+        } else {
+            const rsponse = await fetch(urlWithSort, options);
+        }
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            if (jsonResponse.businesses) {
+                    let returnInfo = await jsonResponse.businesses.map(business => ({
                         id: business.id,
                         imageSrc: business.image_url,
                         name: business.name,
@@ -25,36 +30,13 @@ async function yelp(searchTerm, location, sortOption, api_key){
                         rating: business.rating,
                         reviewCount: business.review_count
                       }));
+                    return returnInfo;
                 }
                 
     
             }
             throw new Error ('Request failed'); 
-        } else {
-            const response = await fetch(`https://api.yelp.com/v3/businesses/search?location=${location}&term=${searchTerm}&sort_by=${sortOption}`, options);
-        if (response.ok) {
-            const jsonResponse = await response.json();
-            if (jsonResponse.businesses) {
-                return jsonResponse.businesses.map(business => ({
-                    id: business.id,
-                    imageSrc: business.image_url,
-                    name: business.name,
-                    address: business.location.address1,
-                    city: business.location.city,
-                    state: business.location.state,
-                    zipCode: business.location.zip_code,
-                    category: business.categories[0].title,
-                    rating: business.rating,
-                    reviewCount: business.review_count
-                  }));
-            }
-            console.log("props.business:", jsonResponse.business);
-            
-
-        }
-        throw new Error ('Request failed');
-        }
-        
+        }         
     }
     catch(error) {
         console.log(error);
